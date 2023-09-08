e4600197125e7a2efdaf972b9f35eeb1d9735cfd
Logan Webster <loganwebster@Logans-Air.rochester.rr.com>
2023-09-07 22:04:52 -0400

    added users to controllers and fixed users in models

--- models/users.js
+++ models/users.js
@@ -1,37 +1,47 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcryptjs');
-const User = require('../models/user.js'); // You should define your User model
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-// User registration route
-router.post('/register', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
+class Exercise extends Model {}
 
-    // Check if the username or email already exists
-    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
-    if (existingUser) {
-      return res.status(400).json({ message: 'Username or email already exists.' });
-    }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user
-    const newUser = new User({
-      username,
-      email,
-      password: hashedPassword,
-    });
-
-    // Save the user to the database
-    await newUser.save();
-
-    res.status(201).json({ message: 'User registered successfully.' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server Error' });
+Exercise.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    exercise_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    assigned_day: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rep_count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    set_count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    workout_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'users',
   }
-});
+);
 
-module.exports = router;
+module.exports = Exercise;
