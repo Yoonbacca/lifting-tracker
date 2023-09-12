16b0e2540b200773f55f5e61b3c09fbdcfbd8c52
Brian Klein <bklein1981@gmail.com>
2023-09-11 20:56:43 -0500

    Implemented seed data to database. Fixed model variables

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,16 +1,36 @@
 const sequelize = require("../config/connection");
-const { Workout } = require("../models");
+const { User, Workout, Exercise } = require("../models/");
 
+const userData = require("./userData.json");
 const workoutData = require("./workoutData.json");
+const exerciseData = require("./exerciseData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const workout = await Workout.bulkCreate(workoutData, {
+    individualHooks: true,
+    returning: true,
+  });
+
   for (const workout of workoutData) {
     await Workout.create({
       ...workout,
+      user_id: users[Math.floor(Math.random() * users.length)].id
     });
   }
+
+  for (const exercise of exerciseData) {
+  await Exercise.create({ 
+    ...exercise,
+   workout_id: workout.id
+  });
+}
 
   process.exit(0);
 };
