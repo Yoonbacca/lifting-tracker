d1900d38ef3d2f2c3337ba9f12a650e096ec0fbb
Brian Klein <bklein1981@gmail.com>
2023-09-11 20:57:53 -0500

    Implemented seed data to database. Fixed model variables

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const User = require('./User');
+const Workout = require('./Workout');
+const Exercise = require('./Exercise');
+
+User.hasMany(Workout, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Workout.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Workout.hasMany(Exercise, {
+    foreignKey: 'workout_id',
+    onDelete: 'CASCADE',
+});
+
+Exercise.belongsTo(Workout, {
+    foreignKey: 'workout_id',
+});
+
+User.hasMany(Exercise, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Exercise.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = { User, Workout, Exercise };
