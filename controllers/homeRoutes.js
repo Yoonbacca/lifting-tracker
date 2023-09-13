1e9dcac28d74c60adbff9faf1e9a4c8d6bd28f7b
YoonJae Kim <yoonjaekim3@gmail.com>
2023-09-12 21:02:56 -0500

    Added homeRoute for workout view and made workout view pull exercise data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
-const { Exercise, User, Workout } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Exercise, User, Workout } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Get all workouts and JOIN with user data
     const workoutData = await Workout.findAll({
       include: [
         {
           model: User,
-          attributes: ['first_name','last_name'],
+          attributes: ["first_name", "last_name"],
         },
       ],
     });
@@ -18,31 +18,36 @@ router.get('/', async (req, res) => {
     const workouts = workoutData.map((workout) => workout.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      workouts, 
-      logged_in: req.session.logged_in 
+    res.render("homepage", {
+      workouts,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/workout/:id', async (req, res) => {
+router.get("/workout/:id", async (req, res) => {
   try {
     const workoutData = await Workout.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["first_name", "last_name"],
+        },
+      ],
+      include: [
+        {
+          model: Exercise,
         },
       ],
     });
 
     const workout = workoutData.get({ plain: true });
 
-    res.render('workout', {
+    res.render("workout", {
       ...workout,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -50,33 +55,33 @@ router.get('/workout/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [{ model: Workout }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render("profile", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
