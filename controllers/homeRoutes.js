48fa60cb4f17ca81b16d7a727f857d47261ff946
Brian Klein <bklein1981@gmail.com>
2023-09-12 19:41:00 -0500

    implemented homeRoute / made changes to homeview handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,82 @@
-const express = require('express');
-const app = express();
-const port = 3001;
+const router = require('express').Router();
+const { Exercise, User, Workout } = require('../models');
+const withAuth = require('../utils/auth');
 
-// Define the home route
-app.get('/', (req, res) => {
-  res.send('Welcome to the home page');
+router.get('/', async (req, res) => {
+  try {
+    // Get all workouts and JOIN with user data
+    const workoutData = await Workout.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['first_name','last_name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const workouts = workoutData.map((workout) => workout.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      workouts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+router.get('/workout/:id', async (req, res) => {
+  try {
+    const workoutData = await Workout.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const workout = workoutData.get({ plain: true });
+
+    res.render('workout', {
+      ...workout,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Workout }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
