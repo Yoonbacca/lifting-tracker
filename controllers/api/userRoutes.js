5300dd929c8f9147954dba9b919f3b5ca9a72b99
Logan Webster <loganwebster@Logans-MacBook-Air.local>
2023-09-14 19:50:26 -0400

    Removed register from userRoutes and fixed loging.js in public directory

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,36 +2,7 @@ const router = require('express').Router();
 
 const { User } = require('../../models'); // You should define your User model
 
-// User registration route
-router.post('/register', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
 
-    // Check if the username or email already exists
-    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
-    if (existingUser) {
-      return res.status(400).json({ message: 'Username or email already exists.' });
-    }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user
-    const newUser = new User({
-      username,
-      email,
-      password: hashedPassword,
-    });
-
-    // Save the user to the database
-    await newUser.save();
-
-    res.status(201).json({ message: 'User registered successfully.' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server Error' });
-  }
-});
 
 router.post('/', async (req, res) => {
   try {
