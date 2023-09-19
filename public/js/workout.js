ff9e21a944f36fa8088739aff6e0c85c6aa227e6
loganrehberg <loganrehberg7@gmail.com>
2023-09-18 20:30:32 -0400

    Made it so you can not edit workout plans if you're not the creator of them via userid tracking

--- public/js/workout.js
+++ public/js/workout.js
@@ -1,10 +1,34 @@
 const addButtonHandler = async (event) => {
-  const currentURL = window.location.href;
-  const urlSegments = currentURL.split("/");
-  // Get the last segment (which should be the workout ID)
-  const id = urlSegments[urlSegments.length - 1];
-  console.log(window.sessionStorage);
-  document.location.replace(`/workout/${id}/exercise`);
+  try {
+    const response = await fetch(`/api/users/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      // Check if the response is not OK (e.g., 500 Internal Server Error)
+      console.error(`Server returned status ${response.status}`);
+      const errorMessage = await response.text(); // Read the error message
+      console.error(`Error message: ${errorMessage}`);
+      return;
+    }
+
+    const currentURL = window.location.href;
+    const urlSegments = currentURL.split("/");
+    const id = urlSegments[urlSegments.length - 1];
+    const creatorUserId = event.target.getAttribute("data-id");
+
+    const loggedInUserId = await response.json();
+    if (loggedInUserId == creatorUserId) {
+      document.location.replace(`/workout/${id}/exercise`);
+    } else {
+      alert("You can only edit workouts you created!");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
 };
 
 document
